e0a23331f176f728498173f6b6ca206192ff7aa9
Kendrix0 <kevin.nguyen9917@gmail.com>
2021-11-09 13:21:52 -0600

    Switched to bootstrap

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,37 @@
 import usdcIcon from './images/usdc_icon.png';
-const content = document.querySelector('#content');
+import bitcoinIcon from './images/bitcoin_icon.png';
+import ethereumIcon from './images/ethereum_icon.png';
+import moneroIcon from './images/monero_icon.png';
+import xlmIcon from './images/xlm_icon.png';
+import algorandIcon from './images/algorand_icon.png';
 
-function createMenuItem(name, image, description, price) {
+const content = document.querySelector('#content');
+const menu = document.createElement('div');
+menu.classList.add('container', 'd-flex', 'justify-content-center', 'flex-wrap','gap-3');
+
+function createMenuItem(name, image, description, price, color) {
     const menuItem = document.createElement('div');
-    menuItem.classList.add('cardContainer')
+    menuItem.classList.add('cardContainer',)
     const menuCard = document.createElement('div');
-    menuCard.classList.add('card');
+    menuCard.classList.add('card',`border-${color}`);
 
     const imageContainer = document.createElement('div');
-    imageContainer.classList.add('card-image');
+    imageContainer.classList.add('card-img');
     const imageFigure = document.createElement('figure');
-    imageFigure.classList.add('image','is-128x128');
+    imageFigure.classList.add('image');
     const imageIcon = new Image();
     imageIcon.src = image;
+    imageIcon.classList.add('mw-100')
     imageFigure.appendChild(imageIcon);
     imageContainer.appendChild(imageFigure);
 
     const cardContent = document.createElement('div');
-    cardContent.classList.add('card-content');
+    cardContent.classList.add('card-body', `bg-${color}`);
     const itemName = document.createElement('p');
-    itemName.classList.add('title', 'is-4');
+    itemName.classList.add('card-title');
     itemName.textContent = name;
     const itemDesc = document.createElement('p');
-    itemDesc.classList.add('subtitle','is-6');
+    itemDesc.classList.add('card-text');
     itemDesc.textContent = description;
     cardContent.appendChild(itemName);
     cardContent.appendChild(itemDesc);
@@ -35,12 +44,20 @@ function createMenuItem(name, image, description, price) {
     menuCard.appendChild(cardContent);
     menuCard.appendChild(cardFooter);
     menuItem.appendChild(menuCard);
-    content.appendChild(menuItem);
+    menu.appendChild(menuItem);
 }
 
 function loadMenu() {
-    content.innerHTML = ''
-    createMenuItem('USDCookie', usdcIcon, 'Price will never change!', '$1.00');
+    content.innerHTML = '';
+
+    createMenuItem('Bitcoin Brownie', bitcoinIcon, 'Only the edge pieces! Most decentralized.', '$100k', 'warning');
+    createMenuItem('Ethereum Eclaire', ethereumIcon, 'Most popular alt-cookie! Expensive delivery.', '$5k', 'primary');
+    createMenuItem('Monero Macaron', moneroIcon, 'Nobody will know you broke your diet!', '$100.00', 'dark');
+    createMenuItem('Stellar Strudels ', xlmIcon, 'Easy to move around!', '$0.40', 'secondary');
+    createMenuItem('Algorand Apple Pie', algorandIcon, 'The recipe will never change!', '$2.00', 'secondary');
+    createMenuItem('USDCookie', usdcIcon, 'The price will never change!', '$1.00', 'primary');
+
+    content.appendChild(menu);
 }
 
 export default loadMenu;
